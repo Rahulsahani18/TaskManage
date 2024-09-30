7a5b80d7c4e1fc60d2337a1acf6fa09bb55c5475
Rahul sahani <153098636+Rahulsahani18@users.noreply.github.com>
2024-09-30 15:27:59 +0530

    Update TaskForm.jsx

--- frontend/src/components/TaskForm.jsx
+++ frontend/src/components/TaskForm.jsx
@@ -73,7 +73,6 @@ const TaskForm = () => {
                     title,
                     description,
                     dueDate,
-                    assignedUser,
                 }, {
                     headers: {
                         Authorization: `Bearer ${token}`,
